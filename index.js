69bc9adb1769c91531f262d3bb5c3c4a9a5e6a44
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2025-03-01 23:44:42 +0600

    changes test

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.status(200).send({ message: "Welcome to ZKTeco Callback Server" });
 });
 
+app.get("/hello", (req, res) => {
+  res.status(200).send({ message: "I love you, Roza" });
+});
+
 // ZKTeco Push Data Endpoint
 app.post("/zkteco-callback", (req, res) => {
   console.log("Received Data from ZKTeco:", req.body);
